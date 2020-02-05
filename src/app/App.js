ceab0647b4811269330d937115968ffef24d86cc
Devin <54780446+barnacleDevelopments@users.noreply.github.com>
2020-02-05 12:58:06 -0400

    Add files via upload

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import Button from '../components/Button';
-import PostsList from '../components/PostsList';
+import { connect } from "react-redux"
+import {createStore, compose} from "redux"
+
+const getMesages = messages => ({
+    type: "GET_MESSAGES",
+    messages: messages
+})
+
+const messageReducer = (state = [], action) => {
+    switch(action.type) {
+        case "GET-MESSAGES":
+        return [...state, action.message]
+        break
+        default:
+            return action.messages
+    }
+}
+
+
+const Store = createStore(messageReducer)
+
+
 
 const App = () => {     
     return(         
     <div>            
-        <Button />            
-        <PostsList />         
+      
+       
     </div>     
 );};
     
